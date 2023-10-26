e75582a12e1c6e506211e2fc86610ec0178fa7a1
Luca Scalzotto <luca@scorpiac.com>
2023-10-26 13:06:19 +0200

    Use PARCELPRO_SHOPSUNITED name in order actions js

--- admin/partials/parcelpro-admin-actions-after.php
+++ admin/partials/parcelpro-admin-actions-after.php
@@ -13,9 +13,9 @@
  */
 
 ?>
-<a href="<?php echo $label_link; ?>" class="button tips parcelpro-label" alt="Print Parcel Pro verzendlabel" data-tip="Print Parcel Pro verzendlabel">
-    <img src="<?php echo dirname(plugin_dir_url(__FILE__)) . '/images/symbol.svg'; ?>" style="height: 16px; width: 12px;" alt="Print Parcel Pro verzendlabel">
+<a href="<?php echo $label_link; ?>" class="button tips parcelpro-label" alt="Print <?= PARCELPRO_SHOPSUNITED ?> verzendlabel" data-tip="Print <?= PARCELPRO_SHOPSUNITED ?> verzendlabel">
+    <img src="<?php echo dirname(plugin_dir_url(__FILE__)) . '/images/symbol.svg'; ?>" style="height: 16px; width: 12px;" alt="Print <?= PARCELPRO_SHOPSUNITED ?> verzendlabel">
 </a>
-<a href="<?php echo $track_link; ?>" class="button tips parcelpro-track" alt="Volg Parcel Pro zending" data-tip="Volg Parcel Pro zending" target="_blank">
-    <img src="<?php echo dirname(plugin_dir_url(__FILE__)) . '/images/transport.svg'; ?>" style="height: 16px; width: 12px;" alt="Volg Parcel Pro zending">
+<a href="<?php echo $track_link; ?>" class="button tips parcelpro-track" alt="Volg <?= PARCELPRO_SHOPSUNITED ?> zending" data-tip="Volg <?= PARCELPRO_SHOPSUNITED ?> zending" target="_blank">
+    <img src="<?php echo dirname(plugin_dir_url(__FILE__)) . '/images/transport.svg'; ?>" style="height: 16px; width: 12px;" alt="Volg <?= PARCELPRO_SHOPSUNITED ?> zending">
 </a>
