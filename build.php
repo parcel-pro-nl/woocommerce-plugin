5ca38acfbe1e3e6e8767c34940f06713dbed3034
Luca Scalzotto <luca@scorpiac.com>
2023-10-26 12:42:27 +0200

    Get version from composer json in build

--- build.php
+++ build.php
@@ -5,25 +5,35 @@ const BOOTSTRAP_TEMPLATE = 'bootstrap.php.hbs';
 const CONFIG_PARCELPRO = 'parcelpro.json';
 const CONFIG_SHOPSUNITED = 'shopsunited.json';
 
+$composerJson = json_decode(file_get_contents('composer.json'), true);
+$composerVersion = $composerJson['version'];
+
 $templateContents = file_get_contents(BOOTSTRAP_TEMPLATE);
 
-$pp = processTemplateWithConfigFile($templateContents, CONFIG_PARCELPRO);
+$ppContext = json_decode(file_get_contents(CONFIG_PARCELPRO), true);
+$ppContext['version'] = $composerVersion;
+$pp = processTemplate($templateContents, $ppContext);
 file_put_contents('woocommerce-parcelpro.php', $pp);
 
-$su = processTemplateWithConfigFile($templateContents, CONFIG_SHOPSUNITED);
+$suContext = json_decode(file_get_contents(CONFIG_SHOPSUNITED), true);
+$suContext['version'] = $composerVersion;
+$su = processTemplate($templateContents, $suContext);
 file_put_contents('woocommerce-shopsunited.php', $su);
 
-function processTemplateWithConfigFile($template, $configFile)
+function processTemplate($template, $context)
 {
     // Get all placeholders that are used in the template.
     preg_match_all('/{{(.*)}}/', $template, $regexOut);
     $keys = array_unique($regexOut[1]);
 
-    $config = json_decode(file_get_contents($configFile), true);
     $result = $template;
 
     foreach ($keys as $key) {
-        $value = $config[$key];
+        if (!isset($context[$key])) {
+            throw new Exception('Undefined template value: "' . $key . '"');
+        }
+
+        $value = $context[$key];
         $result = str_replace('{{' . $key . '}}', $value, $result);
     }
 
