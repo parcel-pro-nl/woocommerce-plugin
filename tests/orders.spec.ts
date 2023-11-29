42eb2d4abc2d7f1eff36714fb5df551b15bed6b1
Luca Scalzotto <luca@scorpiac.com>
2023-11-29 14:18:29 +0100

    Add bulk register order action to test (#29)

--- tests/orders.spec.ts
+++ tests/orders.spec.ts
@@ -45,13 +45,27 @@ test('order meta box actions', async ({ page }) => {
   await expect(page.getByText(/^3S/)).toBeVisible();
 });
 
-test('order bulk actions are available', async ({ page }) => {
+test('order bulk actions', async ({ page }) => {
   await page.goto('/wp-admin');
   await navigateToOrders(page);
-  await createNewOrder(page);
-  await navigateToOrders(page);
+  const orderNumber = await createNewOrder(page);
 
+  await navigateToOrders(page);
+  let orderRow = page.getByRole('row', { name: `#${orderNumber}` });
+
+  await orderRow.getByRole('checkbox').click();
+
+  // Check if all bulk actions are available, select the register order action.
   const selectField = page.locator('#bulk-action-selector-top');
-  await selectField.selectOption('Aanmelden bij Parcel Pro');
   await selectField.selectOption('Print Parcel Pro label');
+  await selectField.selectOption('Aanmelden bij Parcel Pro');
+
+  // Click the apply button.
+  await page.locator('#doaction').click();
+
+  // Check if the order is registered.
+  orderRow = page.getByRole('row', { name: `#${orderNumber}` });
+  await expect(
+    orderRow.getByRole('link', { name: 'Volg Parcel Pro zending' }),
+  ).toBeVisible();
 });
