d8d71e6efbc7794d41c0c25b14e4dc9fad773eaf
Luca Scalzotto <luca@scorpiac.com>
2023-11-30 16:02:49 +0100

    Use username and password to get api info from PP platform

--- tests/configure.setup.ts
+++ tests/configure.setup.ts
@@ -3,6 +3,15 @@ import { navigateWooCommerce } from './helpers/navigate';
 import { createNewProduct, navigateToProducts } from './helpers/products';
 
 test('configure plugin', async ({ page }) => {
+  // Get the user id and api key from Parcel Pro.
+  await page.goto('https://login.parcelpro.nl/koppeling/single.php?type=api');
+  const userId = await page
+    .locator('[data-bind="value: LoginId "]')
+    .inputValue();
+  const apiKey = await page
+    .locator('[data-bind="value: ApiKey "]')
+    .inputValue();
+
   await page.goto('/wp-admin');
   await navigateWooCommerce(page, 'Settings');
 
@@ -16,10 +25,8 @@ test('configure plugin', async ({ page }) => {
   }
 
   // Fill the user id and api key fields.
-  await page
-    .getByLabel('Gebruikers Id', { exact: true })
-    .fill(process.env.USER_ID);
-  await page.getByLabel('API Key', { exact: true }).fill(process.env.API_KEY);
+  await page.getByLabel('Gebruikers Id', { exact: true }).fill(userId);
+  await page.getByLabel('API Key', { exact: true }).fill(apiKey);
 
   await page.getByRole('button', { name: 'Save changes' }).click();
 });
