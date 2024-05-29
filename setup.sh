6322acd25ce43e409e27f31c2774ac9ec772a537
Luca Scalzotto <luca@scorpiac.com>
2024-05-29 11:55:33 +0200

    Dev setup improvements (#44)

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ docker run --rm --volumes-from wordpress --network container:wordpress --user 33
   -e WORDPRESS_DB_PASSWORD=wordpress \
   -e WORDPRESS_DB_NAME=wordpress \
   -v './setup-in-docker.sh:/usr/src/setup.sh' \
+  -v './.env:/usr/src/.env' \
   wordpress:cli bash /usr/src/setup.sh "$@"
